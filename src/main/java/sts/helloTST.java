92835587885aa0ef2f64758ad1b75ba635116444
lian <1244583752@qq.com>
2020-08-08 16:55:06 +0800

    从github引入sts中第一次提交

--- src/main/java/sts/helloTST.java
+++ src/main/java/sts/helloTST.java
@@ -4,5 +4,6 @@ public class helloTST {
 	public static void main(String[] args) {
 		System.out.println("第一个sts项目推送到github");
 		System.out.println("更新sts项目");
+		System.out.println("从github引入到sts第一次提交");
 	}
 }
